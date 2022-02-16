7e255929c2d54462b75dfe24bb98c2b10b09e1c5
Jet110390 <terryedwardsjr113@gmail.com>
2022-02-16 15:41:15 -0500

    still working

---
+++ app/src/main/java/com/example/usershilt02_14_22/features/user/view/UsersFragment.kt
@@ -0,0 +1,52 @@
+package com.example.usershilt02_14_22.features.user.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.usershilt02_14_22.databinding.FragmentUserBinding
+import com.example.usershilt02_14_22.features.user.adapter.UserAdapter
+import com.example.usershilt02_14_22.features.user.viewmodel.UsersViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+
+class UsersFragment: Fragment() {
+
+    private var _binding: FragmentUserBinding? = null
+    private val binding: FragmentUserBinding get() = _binding!!
+
+    private val viewModel: UsersViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentUserBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        with(binding) {
+            viewModel.users.observe(viewLifecycleOwner) {users->
+                userRv.apply {
+                    adapter = users?.let { UserAdapter(it) }
+                    layoutManager =
+                        LinearLayoutManager(requireContext())
+                }
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
